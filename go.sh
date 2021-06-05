013ed46887b8c7b34857a870a99b8c9bb41e56fd
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-06-06 01:18:47 +0700

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,14 @@
+cd ~
+curl -O https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
+sha256sum go1.10.3.linux-amd64.tar.gz
+tar xvf go1.10.3.linux-amd64.tar.gz
+sudo chown -R root:root ./go
+sudo mv go /usr/local
+sudo cat >> ~/.profile <<EOF
+{
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+}
+EOF
+source ~/.profile
+
