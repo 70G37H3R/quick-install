e92415dfaf841952de799961a63e3efe6c571a79
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-06-06 01:24:26 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,6 +2,7 @@
 #DO NOT RUN THIS SCRIPT AS ROOT#
 ################################
 #Install Docker and Docker-Compose
+cd ~
 sudo apt update
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
