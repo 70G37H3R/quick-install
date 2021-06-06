e65185287ef2d87e0bc95f49b19526d9fd74c587
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-06-06 14:27:00 +0700

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,3 +1,5 @@
 sudo apt install firewalld -y
-firewall-cmd --permanent --zone=public --add-port=2377/tcp --add-port=7946/tcp --add-port=7946/udp --add-port=4789/udp
-firewall-cmd --reload
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --zone=public --add-port=2377/tcp --add-port=7946/tcp --add-port=7946/udp --add-port=4789/udp
+sudo firewall-cmd --reload
+sudo firewall-cmd --list-all
