cecf2896ef6d43fad532c01e0686a2155cb7df3b
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-06-14 03:16:51 +0700

    Update and rename firewall.sh to nodejs.sh

--- firewall.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install firewalld -y
-sudo systemctl start firewalld
-sudo firewall-cmd --permanent --zone=public --add-port=2377/tcp --add-port=7946/tcp --add-port=7946/udp --add-port=4789/udp
-firewall-cmd --permanent --zone=public --add-rich-rule='rule family=ipv4 source address=172.27.0.0/16 accept'
-sudo firewall-cmd --reload
-sudo firewall-cmd --list-all
