35f1b8aa9025570cd46b79d138d37dbf7ce145d1
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-06-23 03:43:08 +0700

    Update python.sh

--- python.sh
+++ python.sh
@@ -2,5 +2,5 @@
 sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.7
+sudo apt install -y python3.7
 python3.7 --version
