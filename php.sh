32ebd54a6e3d5dc2f1eea110d6c041597d2388e5
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-06-23 04:05:19 +0700

    Create php.sh

---
+++ php.sh
@@ -0,0 +1,5 @@
+#PHP 7.3
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt install -y php7.3 php7.3-common php7.3-opcache php7.3-cli php7.3-gd php7.3-curl php7.3-mysql
+php -v
