4e6aee3891713f58c1975f93fbb29a9c5fd18058
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-07-04 14:59:20 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,6 @@
 #GO 1.3
 cd ~
+sudo apt update
 sudo apt instal -y wget 
 wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
