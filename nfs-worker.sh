aa5eadf806e28b1d0a337d25485248a3649f326a
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-07-25 04:56:38 +0700

    Update nfs-worker.sh

--- nfs-worker.sh
+++ nfs-worker.sh
@@ -1,6 +1,6 @@
 sudo apt install -y nfs-common
-sudo mkdir -p /shared
-sudo mount 10.62.157.209:/shared /shared
+sudo mkdir -p ~/shared
+sudo mount 10.62.157.209:~/shared ~/shared
 sudo tee /etc/fstab <<EOF
 10.62.157.209:/shared /shared auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0
 EOF
