68a09f70d563d15309bcf6b7c7d0068e2f198fc1
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-07-25 04:56:18 +0700

    Update nfs-master.sh

--- nfs-master.sh
+++ nfs-master.sh
@@ -1,9 +1,9 @@
 sudo apt install -y nfs-kernel-server
-sudo mkdir -p /shared
-sudo chown nobody:nogroup /shared
-sudo chmod 777 /shared
+sudo mkdir -p ~/shared
+sudo chown nobody:nogroup ~/shared
+sudo chmod 777 ~/shared
 sudo tee /etc/exports <<EOF
-/shared      *(rw,sync,no_subtree_check,no_root_squash)
+~/shared      *(rw,sync,no_subtree_check,no_root_squash)
 EOF
 sudo exportfs -a
 sudo systemctl restart nfs-kernel-server
