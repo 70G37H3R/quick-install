79211118b9eea5d27c925aee80a5b8981fa2b578
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-07-25 23:47:48 +0700

    Update nfs-worker.sh

--- nfs-worker.sh
+++ nfs-worker.sh
@@ -1,6 +1,6 @@
 sudo apt install -y nfs-common
 sudo mkdir -p ~/shared
-sudo mount 10.62.157.209:~/shared ~/shared
+sudo mount 192.168.100.219:/nfs/fabric /nfs/fabric
 sudo tee /etc/fstab <<EOF
-10.62.157.209:/shared /shared auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0
+192.168.100.219:/nfs/fabric /nfs/fabric auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0
 EOF
