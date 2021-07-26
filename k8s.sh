bf004e03b3b5166e2c0e4b781e31deb2b509f5bb
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-07-26 07:47:00 +0700

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -2,16 +2,19 @@ sudo apt update
 sudo apt install -y curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt update
 sudo apt-get install -y kubeadm kubelet kubectl 
 sudo apt-mark hold kubeadm kubelet kubectl
 kubeadm version
 sudo swapoff –a
-sudo kubeadm init --pod-network-cidr=10.0.0.0/24
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sudo kubectl get nodes
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-sudo kubectl get pods --all-namespaces
+sudo export KUBECONFIG=/etc/kubernetes/kubelet.conf
+
+#sudo kubectl get nodes
+#sudo kubectl get pods --all-namespaces
+#sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 #kubectl taint nodes --all node-role.kubernetes.io/master-
