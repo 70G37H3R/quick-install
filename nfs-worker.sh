2014fb26b094cdbe62b24fdc1043adc8c688ffea
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-08-04 06:14:43 +0700

    Delete nfs-worker.sh

--- nfs-worker.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y nfs-common
-sudo mkdir -p /nfs/fabric
-sudo mount 192.168.100.219:/nfs/fabric /nfs/fabric
-sudo tee /etc/fstab <<EOF
-192.168.100.219:/nfs/fabric /nfs/fabric auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0
-EOF
