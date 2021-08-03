92021ce395d2d053ab86d0b02c516e6e64580ce6
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-08-04 06:14:37 +0700

    Delete nfs-master.sh

--- nfs-master.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install -y nfs-kernel-server
-sudo mkdir -p /nfs/fabric
-sudo chown nobody:nogroup /nfs/fabric
-sudo chmod 777 /nfs/fabric
-sudo tee /etc/exports <<EOF
-/nfs/fabric      *(rw,sync,no_subtree_check,no_root_squash)
-EOF
-sudo exportfs -a
-sudo systemctl restart nfs-kernel-server
