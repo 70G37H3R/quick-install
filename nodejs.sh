db834262bdf88a2918ba33294785c569c4206740
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-08-07 23:11:22 +0700

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -6,7 +6,6 @@ sudo apt install -y software-properties-common
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt update
 sudo apt install -y nodejs
-sudo apt install -y npm
 
 node --version
 npm --version
