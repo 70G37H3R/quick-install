09a2e44b0d96809f90f603f80cd09e7c9df14759
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-08-24 15:45:12 +0700

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -6,8 +6,8 @@ sudo apt update
 sudo apt-get install -y kubeadm kubelet kubectl 
 sudo apt-mark hold kubeadm kubelet kubectl
 kubeadm version
-sudo kubeadm init --pod-network-cidr=10.0.0.0/24
 swapoff –a
+sudo kubeadm init --pod-network-cidr=10.0.0.0/24
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
