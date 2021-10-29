6d3445a8acdf37ab36bb7a4c15fd65182636cc18
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-10-30 03:30:29 +0700

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt install -y build-essential
 sudo apt install -y software-properties-common
 sudo apt install -y gcc g++ make
+npm install github:barrysteyn/node-scrypt#fb60a8d3c158fe115a624b5ffa7480f3a24b03fb
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt update
