0cc412e0d1c2feacef3115323e7a9843e7eaacc7
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-10-30 14:01:02 +0700

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -5,7 +5,7 @@ sudo apt install -y software-properties-common
 sudo apt install -y gcc g++ make
 npm install github:barrysteyn/node-scrypt#fb60a8d3c158fe115a624b5ffa7480f3a24b03fb
 npm install -g truffle@4.1.14
-
+npm install web3
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt update
