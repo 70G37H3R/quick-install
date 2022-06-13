86439c58f0fd60b10b204ed5247a603060ce3937
70G37H3R <nchihminh@gmail.com>
2022-06-13 15:02:06 +0700

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,4 +1,5 @@
 sudo apt update
+sudo apt-get install -y conntrack socat
 sudo apt install -y curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
