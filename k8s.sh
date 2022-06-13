5787b9a8701f4375fa921405234b3d243933ef38
70G37H3R <nchihminh@gmail.com>
2022-06-13 14:55:55 +0700

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -15,6 +15,8 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+#rm /etc/containerd/config.toml
+#systemctl restart containerd
 
 #sudo kubectl get nodes
 #sudo kubectl get pods --all-namespaces
